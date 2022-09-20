0f056ef77749ada15aa5eac29af033fc5d61213c
BotkovaZ <464686@student.saxion.nl>
2022-09-21 00:53:58 +0200

    register activity

--- MagicFridge/app/src/main/java/com/example/magicfridge/MainActivity.kt
+++ MagicFridge/app/src/main/java/com/example/magicfridge/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.magicfridge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.magicfridge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding : ActivityMainBinding
+    lateinit var name : EditText
+    lateinit var password : EditText
+    lateinit var login : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
+
+        login.setOnClickListener {
+            var name : String = name.text.toString()
+            var password : String = password.text.toString()
+
+            var intent = Intent(this@MainActivity, RegisterActivity::class.java)
+
+        }
     }
 }
